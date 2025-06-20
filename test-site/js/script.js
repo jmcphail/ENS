cc0dd8af2df30e01b07860580b4b5337e0f86f7e
LukeEPereira <luke_pereira@outlook.com>
2025-06-20 12:38:32 -0700

    Testing RUNSAM

--- test-site/js/script.js
+++ test-site/js/script.js
@@ -2,3 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const output = document.getElementById("output");
   output.textContent = "JavaScript is working! Edit js/script.js to get started. Luke is Testing if he can make changes";
 });
+
+//Testing RunSam
+const myH1 = document.getElementById("myH1");
+myH1.textContent = "This Test Succeeded!";
+
