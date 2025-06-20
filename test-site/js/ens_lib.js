5589ea359d35d131a00356a9d31695fa4907e1cb
James McPhail <james@isti.ca>
2025-06-20 13:46:01 -0700

    dial it in

--- test-site/js/ens_lib.js
+++ test-site/js/ens_lib.js
@@ -2,12 +2,3 @@ document.addEventListener("DOMContentLoaded", function () {
   const superH3 = document.getElementById("superH3");
   superH3.textContent = "JavaScript is working!";
 });
-
-//Testing RunSam
-
-const myH1 = document.getElementById("myH1");
-const testButton = document.getElementById("testButton");
-
-testButton.onclick = function(){
-  myH1.textContent = "This Test";
-}
