b9889318fdd13f877901e7a7e72e82620202cce8
LukeEPereira <luke_pereira@outlook.com>
2025-06-20 13:29:22 -0700

    Testing button in RUNSAM

--- test-site/js/script.js
+++ test-site/js/script.js
@@ -1,9 +1,17 @@
+// use <script src="https://cdn.jsdelivr.net/gh/jmcphail/ENS/test-site/js/script.js"></script> in RUNSAM
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const output = document.getElementById("output");
-  output.textContent = "JavaScript is working! Edit js/script.js to get started. Luke is Testing if he can make changes";
+  output.textContent = "JavaScript is working! Edit js/script.js to get started.";
 });
 
 //Testing RunSam
 const myH1 = document.getElementById("myH1");
-myH1.textContent = "This Test Succeeded!";
+const testButton = document.getElementById("testButton");
 
+testButton.onclick = function(){
+  myH1.textContent = "This Test";
+}
