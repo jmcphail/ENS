2f6dbd9eaf811f5fd0e9863f9152e4202c93d327
LukeEPereira <luke_pereira@outlook.com>
2025-06-24 11:27:45 -0700

    integrated basic chart colouring (will probably change later)

--- test-site/js/script.js
+++ test-site/js/script.js
@@ -16,7 +16,7 @@ const monthlyEnergyButton = document.getElementById("monthlyEnergyButton");
 const yearlyEnergyButton = document.getElementById("yearlyEnergyButton");
 
 let isVisible = true;
-
+const BRIGHTNESSCHANGE = 15;
 function energyChartVisibility(){
   if(isVisible){
     energyGenElements.classList.remove('show');
@@ -29,6 +29,26 @@ function energyChartVisibility(){
     isVisible = true;
   }
 }
+function getLargest(previous, next){
+    return Math.max(previous, next);
+}
+function getBackgroundColor(data){
+    let i;
+    let colorArray = [];
+    const largest = data.reduce(getLargest);
+    console.log(largest);
+    for(i = 0; i < data.length; i++){
+        const colorPercent = 50-(BRIGHTNESSCHANGE*(data[i]/largest));
+        console.log(colorPercent);
+        chartColor = `hsl(96,100%,${colorPercent}%)`;
+        chartColor = String(chartColor);
+        colorArray.push(chartColor);
+    }
+    console.log(colorArray);
+    return colorArray;
+}
+const sampleData = [12, 5, 3, 5, 2, 3];
+const backgroundColors = getBackgroundColor(sampleData)
 async function fetchEnergyData() {
       const response = await fetch("https://clients.hakaienergy.ca/camosun/get_site_energy.php?t=DAY&r=-30%20days&f=json");
       const data = await response.json();
@@ -53,7 +73,7 @@ async function fetchEnergyData() {
           datasets: [{
             label: "Daily Energy (Wh)",
             data: values,
-            backgroundColor: "rgba(54, 162, 235, 0.6)",
+            backgroundColor: getBackgroundColor(values),
             borderColor: "rgba(54, 162, 235, 1)",
             borderWidth: 1,
             maintainAspectRatio: false
